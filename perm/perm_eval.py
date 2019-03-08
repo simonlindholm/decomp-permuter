8a47c8cac33385fe84e9fdb64d04e124dece75fc
Simon Lindholm <simon.lindholm10@gmail.com>
2019-03-08 15:15:16 +0100

    Get rid of hard-coded limit in get_all_seeds

--- perm/perm_eval.py
+++ perm/perm_eval.py
@@ -1,19 +1,26 @@
-from typing import List, Iterable
+from typing import List, Iterable, Set
 import math
 import random
 
 from perm.perm import Perm
 
 def get_all_seeds(total_count: int) -> Iterable[int]:
-    if total_count > 10**5:
-        while True:
-            yield random.randrange(total_count)
-    else:
-        # General list of possible combinations
-        all_seeds = list(range(total_count))
-        random.shuffle(all_seeds)
-        for seed in all_seeds:
+    """Generate all numbers 0..total_count-1 in random order, in expected time
+    O(1) per number."""
+    seen: Set[int] = set()
+    while len(seen) < total_count // 2:
+        seed = random.randrange(total_count)
+        if seed not in seen:
+            seen.add(seed)
             yield seed
+
+    remaining: List[int] = []
+    for seed in range(total_count):
+        if seed not in seen:
+            remaining.append(seed)
+    random.shuffle(remaining)
+    for seed in remaining:
+        yield seed
 
 def perm_evaluate_all(perm: Perm) -> Iterable[str]:
     while True:
