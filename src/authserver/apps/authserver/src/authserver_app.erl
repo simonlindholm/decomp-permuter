b82129b0a2ce74c8f8100b00ba178a23bfd51ff6
Matt Kempster <kidpixel@gmail.com>
2020-06-28 13:25:02 -0700

    Add initial erlang webserver for auth

---
+++ src/authserver/apps/authserver/src/authserver_app.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%% @doc authserver public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(authserver_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    {ok, Pid} = authserver_sup:start_link(),
+    Dispatch = cowboy_router:compile([{
+        '_',
+        [
+            {"/", route_root, []}
+        ]
+    }]),
+
+    TransOpts = [ {ip, {0,0,0,0}}, {port, 2938} ],
+    ProtoOpts = #{env => #{dispatch => Dispatch}},
+
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        TransOpts, ProtoOpts),
+
+    {ok, Pid}.
+
+
+stop(_State) ->
+    ok.
+
+%% internal functions
---
+++ src/authserver/apps/authserver/src/route_root.erl
@@ -0,0 +1,10 @@
+-module(route_root).
+
+-export([init/2]).
+
+init(Req, Opts) ->
+    Req2 = cowboy_req:reply(200,
+        #{<<"content-type">> => <<"text/plain">>},
+        <<"Welcome to the decomp-permuter authserver.">>,
+        Req),
+    {ok, Req2, Opts}.
