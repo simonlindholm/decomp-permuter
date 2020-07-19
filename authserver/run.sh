cf81ac160551bac827e125deac4e21611731c34a
Simon Lindholm <simon.lindholm10@gmail.com>
2020-07-19 15:18:53 +0200

    [authserver] fix configuration loading

--- authserver/run.sh
+++ authserver/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 rebar3 compile
+CMDLINE="erl -pa _build/default/lib/*/ebin -s authserver_app -config config/sys.config $(cat config/vm.args)"
 if [ "$1" = "-daemon" ]; then
-    run_erl -daemon /tmp/ /tmp/ "exec erl -pa _build/default/lib/*/ebin -s authserver_app"
+    run_erl -daemon /tmp/ /tmp/ "exec $CMDLINE"
 else
-    erl -pa _build/default/lib/*/ebin -s authserver_app "$@"
+    $CMDLINE "$@"
 fi
