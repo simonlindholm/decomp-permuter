25ba015e2a7b0bd2244e2df21b8cf971b05aafc1
Simon Lindholm <simon.lindholm10@gmail.com>
2020-12-06 22:31:45 +0100

    Remove setUpClass from test runner

--- test/test_perm.py
+++ test/test_perm.py
@@ -11,45 +11,27 @@ from src.compiler import Compiler
 from src.preprocess import preprocess
 from src import main
 
-c_files_list = [
-    ["test_general.c", "test_general"],
-    ["test_general.c", "test_general_3"],
-    ["test_general.c", "test_general_multiple"],
-    ["test_ternary.c", "test_ternary1"],
-    ["test_ternary.c", "test_ternary2"],
-    ["test_type.c", "test_type1"],
-    ["test_type.c", "test_type2"],
-    ["test_type.c", "test_type3"],
-    ["test_ignore.c", "test_ignore"],
-    ["test_randomizer.c", "test_randomizer"],
-    ["test_randomizer.c", "test_randomizer2"],
-    ["test_once.c", "test_once1"],
-    ["test_once.c", "test_once2"],
-]
-
 
 class TestStringMethods(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+    def go(self, filename, fn_name, **kwargs) -> int:
         compiler = Compiler("test/compile.sh")
-        cls.tmp_dirs = {}
-        for test_c, test_fn in c_files_list:
-            d = tempfile.TemporaryDirectory()
-            file_test = os.path.join("test", test_c)
-            file_actual = os.path.join(d.name, "actual.c")
-            file_base = os.path.join(d.name, "base.c")
-            file_target = os.path.join(d.name, "target.o")
+
+        with tempfile.TemporaryDirectory() as target_dir:
+            file_test = os.path.join("test", filename)
+            file_actual = os.path.join(target_dir, "actual.c")
+            file_base = os.path.join(target_dir, "base.c")
+            file_target = os.path.join(target_dir, "target.o")
 
             actual_preprocessed = preprocess(file_test, cpp_args=["-DACTUAL"])
             base_preprocessed = preprocess(file_test, cpp_args=["-UACTUAL"])
 
             # Strip away other functions to be able to get an .o file with
             # only the target function to compare again.
-            strip_other_fns_and_write(actual_preprocessed, test_fn, file_actual)
+            strip_other_fns_and_write(actual_preprocessed, fn_name, file_actual)
 
             # For symmetry, do the same for the base file. This isn't technically
             # necessary.
-            strip_other_fns_and_write(base_preprocessed, test_fn, file_base)
+            strip_other_fns_and_write(base_preprocessed, fn_name, file_base)
 
             actual_source = Path(file_actual).read_text()
             target_o = compiler.compile(actual_source, show_errors=True)
@@ -57,18 +39,10 @@ class TestStringMethods(unittest.TestCase):
             shutil.copy2(target_o, file_target)
             os.remove(target_o)
 
-            shutil.copy2("test/compile.sh", d.name)
-            cls.tmp_dirs[(test_c, test_fn)] = d
+            shutil.copy2("test/compile.sh", target_dir)
 
-    @classmethod
-    def tearDownClass(cls):
-        for d in cls.tmp_dirs.values():
-            d.cleanup()
-
-    def go(self, filename, fn_name, **kwargs) -> int:
-        d = self.tmp_dirs[(filename, fn_name)].name
-        (score,) = main.run(main.Options(directories=[d], stop_on_zero=True, **kwargs))
-        return score
+            opts = main.Options(directories=[target_dir], stop_on_zero=True, **kwargs)
+            return main.run(opts)[0]
 
     def test_general(self):
         score = self.go("test_general.c", "test_general")
