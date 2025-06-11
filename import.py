b3320625af19fb2b578ebd90f7407206a344c128
Ethan <ethteck@gmail.com>
2025-06-11 19:35:21 +0900

    Special user agent

--- import.py
+++ import.py
@@ -974,9 +974,7 @@ def main(arg_list: List[str]) -> None:
             req = urllib.request.Request(
                 f"{api_base}/api/scratch",
                 data=post_data,
-                headers={
-                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
-                },
+                headers={"User-Agent": "Decomp-Permuter"},
             )
             with urllib.request.urlopen(req) as f:
                 resp = f.read()
